f7928f418393b0650cc208fc219d0196569ff30b
SuinShin <148019115+SuinShin@users.noreply.github.com>
2024-01-14 00:30:00 +0900

    [level Lv.0] Title: 양꼬치, Time: 0.01 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int service = n/10;
+        
+        if(service >= 1) {
+            answer = (n*12000)+(k*2000)-(service*2000);
+        }else {
+            answer = (n*12000)+(k*2000);
+        }
+        
+        return answer;
+    }
+}
